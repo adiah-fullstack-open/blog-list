1cd5f38bf8fb18f159a4b9c924eef35426c086aa
nathanieladiah <nathanieladiah@gmail.com>
2023-07-02 20:41:12 -0400

    Import application from app.js

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-const express = require("express");
-const app = express();
-const cors = require("cors");
+const app = require("./app");
 
-const mongoose = require("mongoose");
 const Blog = require("./models/blog");
 
 require("dotenv").config();
-
-const mongoUrl = config.MONGODB_URI;
-mongoose
-  .connect(mongoUrl)
-  .then(() => logger.info("Connected to MongoDB"))
-  .catch((error) => {
-    logger.error(error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
 
 app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -36,7 +22,6 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = config.PORT;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
