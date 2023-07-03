54624270379d65eed5c367b3a53f013437eb64b1
nathanieladiah <nathanieladiah@gmail.com>
2023-07-02 20:59:58 -0400

    Routes are now defined via the blogsRouter

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
+
+const blogsRouter = require("./controllers/blogs");
+
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
@@ -20,5 +23,7 @@ mongoose
 app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,26 +1,6 @@
+const app = require("./app");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-
-const app = require("./app");
-
-const Blog = require("./models/blog");
-
-require("dotenv").config();
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  // const body =
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
