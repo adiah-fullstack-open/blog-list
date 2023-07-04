35876bb18214275a89f43ef7787db9eaae5a81e3
nathanieladiah <nathanieladiah@gmail.com>
2023-07-04 07:35:32 -0400

    Refactor route handlers to use async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,31 +1,16 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response, next) => {
-  // const body = request.body
-
-  // const blog = new Blog({
-
-  // })
-
+blogsRouter.post("/", async (request, response, next) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-
-  //   blog
-  //     .save()
-  //     .then((savedNote) => {
-  //       response.json(savedNote);
-  //     })
-  //     .catch((error) => next(error));
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
