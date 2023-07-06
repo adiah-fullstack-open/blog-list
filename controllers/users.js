821e9545eaff7b7b081fc2ceff59abf0bc258c52
nathanieladiah <nathanieladiah@gmail.com>
2023-07-06 14:05:54 -0400

    Implement validations when creating a new user

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,12 @@ const User = require("../models/user");
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (password.length < 3) {
+    return response.status(400).json({
+      error: "Password must be at least 3 characters",
+    });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
@@ -17,6 +23,15 @@ usersRouter.post("/", async (request, response) => {
   const savedUser = await user.save();
 
   response.status(201).json(savedUser);
+});
+
+usersRouter.get("/", async (requests, response) => {
+  // const users = await User.find({}).populate("notes", {
+  //   content: 1,
+  //   important: 1,
+  // });
+  const users = await User.find({});
+  response.json(users);
 });
 
 module.exports = usersRouter;
--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -176,6 +176,56 @@ describe("Adding users when there is initially one user in db", () => {
     const usernames = usersAtEnd.map((u) => u.username);
     expect(usernames).toContain(newUser.username);
   });
+
+  test("creation fails when password is too short", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "shortPassword",
+      name: "short Pass",
+      password: "ts",
+    };
+
+    await api.post("/api/users").send(newUser).expect(400);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toEqual(usersAtStart);
+  });
+
+  test("creation fails when no username or password is provided", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      name: "short Pass",
+      password: "ts",
+    };
+
+    await api.post("/api/users").send(newUser).expect(400);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toEqual(usersAtStart);
+  });
+
+  test("creation fails if username is already taken", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "root",
+      name: "Superuser",
+      password: "salainen",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    expect(result.body.error).toContain("expected `username` to be unique");
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toEqual(usersAtStart);
+  });
 });
 
 afterAll(async () => {
